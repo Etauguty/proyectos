4b2b2a11affc104c1a4e64f66bc004095794cbfb
Etauguty <etauguty@gmail.com>
2021-03-20 18:09:07 -0700

    todo list

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app" class="container">
+    <h2 style="text-align:left">LKMX - Front-end</h2>
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+
+.container{
+  max-width: 600px;
+  max-height: 600px;
+  margin: 0 auto;
+  background-color: #E7E7E7;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 50px;
+  font-size: 24px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="containerTodo">
+        <h5 style="text-align:left">To Do List</h5>
+        <input type="text" class="todo-input" placeholder="Escribe una tarea" v-model="newTodo" @keyup.enter="addTodo()">
+        <button type="submit" class="submit-button" @click="addTodo()">Agregar</button>
+
+        <div v-for="(todo,index) in todos" :key="todo.id" class="todo-item">
+            <div class="todo-item-left">             
+                <div v-if="!todo.editing" class="todo-item-label"><li>{{todo.title}}</li></div>
+                <input v-else class="todo-item-edit" type="text" v-model="todo.title" 
+                @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" 
+                @keyup.esc="cancelEdit(todo)" v-focus>
+                <img v-if="(todo.editing == true)" @click="addTodo()" src="../assets/icon-disk.svg">
+            </div>
+            <div class="items">
+                <img @click="editTodo(todo)" src="../assets/icon-pen.svg">
+                <img @click="removeTodo(index)" src="../assets/icon-trash.svg">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoList',
+    data () {
+        return{
+            newTodo: '',
+            idForTodo: 3,
+            beforeEditCache : '',
+            todos: [
+                {
+                    'id':1,
+                    'title': 'Task 01',
+                    'completed': false,
+                    'editing': false,
+                },
+                {
+                    'id':2,
+                    'title': 'Task 02',
+                    'completed': false,
+                    'editing': false,
+                }
+            ]
+        }
+    },
+
+    directives: {
+        focus: {
+            inserted: function (el) {
+            el.focus()
+            }
+        }
+    },
+
+    methods:{
+        //Método para agregar tareas
+        addTodo(){
+            //Checa que no se agreguen espacios en blanco
+            if(this.newTodo.trim().length == 0){
+                return
+            }
+
+            this.todos.push({
+                id: this.idForTodo,
+                title: this.newTodo,
+                completed: false,
+                editing: false,
+            })
+
+            this.newTodo = ''
+            this.idForTodo++
+        },
+        //Método para editar tareas
+        editTodo(todo){
+            this.beforeEditCache = todo.title
+            todo.editing = true;
+        },
+        //Método para 
+        doneEdit(todo){
+            if(todo.title.trim() == ''){
+                todo.title = this.beforeEditCache
+            }
+            todo.editing = false;
+        },
+        cancelEdit(todo){
+            todo.title = this.beforeEditCache;
+            todo.editing = false;
+        },
+        removeTodo(index){
+            this.todos.splice(index, 1)
+        }
+    }
+}
+</script>
+
+<style>
+    /*Ajusta al contendedor*/
+    *{
+        box-sizing: border-box;
+    }
+
+    /*Edita el contenedor principal*/
+    .containerTodo{
+    max-width: 550px;
+    max-height: 550px;
+    margin: 0 auto;
+    background-color:#E7E7E7;
+    padding: 5px;
+    padding-bottom: 50px;
+    }
+
+    /*Edita el área de ingreso de texto*/
+    .todo-input{
+        width: 81%;
+        padding: 10px 10px;
+        font-size: 14px;
+        margin-bottom: 16px;
+    }
+
+    /*Edita el área de tareas*/
+    .todo-item{
+        margin: auto;
+        padding: 10px;
+        font-size: 14px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: lightgrey;
+    }
+    
+    /*Edita el área de tareas al momento de editar*/
+    .todo-item-edit{
+        font-size: 14px;
+        color: #2c3e50;
+        margin-left: 12px;
+        width: 100%;
+        display: inline-block;
+        padding: 18px;
+        border-color: red;
+    }
+    
+    /*Edita el boton de agregar*/
+    .submit-button {
+        background: #0066A2;
+        color: white;
+        border-style: outset;
+        border-color: #0066A2;
+        height: 40px;
+        width: 100px;
+        font: bold 15px arial, sans-serif;
+        text-shadow:none;
+    }
+
+    /*Edita ela rea de los botones para editar y eliminar*/
+    .items{
+        margin: 5px;
+    }
+</style>
